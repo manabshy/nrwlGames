5d453dbaa2c09a00672cf4cb7d2bdb05dcc845ad
MJ B <mj@MJs-MacBook-Pro.local>
2020-09-23 21:46:00 +0100

    Add library shared-ui

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { StoreSharedUiModule } from '@bg-board/store/shared-ui';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    MatCardModule
+    MatCardModule,
+    StoreSharedUiModule
   ],
   providers: [],
   bootstrap: [AppComponent],
